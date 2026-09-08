c4561900b03904f32c403e10cc8d8a3eb0fa3177
JoeMartini <joe_martini@msn.com>
2026-09-08 12:58:54 +0800

    test: add source-table to integration repro query

--- metabase-maxcompute-driver/test/metabase/driver/maxcompute_test.clj
+++ metabase-maxcompute-driver/test/metabase/driver/maxcompute_test.clj
@@ -51,7 +51,8 @@
        (qp.compile/compile
          {:database 1
           :type     :query
-          :query    {:fields [[:field 1001 nil] [:field 1002 nil]]}})))))
+          :query    {:source-table 100
+                   :fields      [[:field 1001 nil] [:field 1002 nil]]}})))))
 
 (deftest integration-table-browse-sql-test
   (testing "table-browse MBQL query compiles to well-formed SQL: every field renders an expression, FROM is two-part"
